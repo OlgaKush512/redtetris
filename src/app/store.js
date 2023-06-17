efd75ef80c54503226c370bddd2cb292079ccc48
Olga Kushnirova <olgakushnirova27@gmail.com>
2023-06-17 22:50:18 +0200

    redux + homepage

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginFormReducer from '../components/loginFormSlice';
 // import inputReducer from '../components/inputSlice';
+import homePageReducer from '../components/homePageSlice';
+
 
 export const store = configureStore({
   reducer: {
     loginForm: loginFormReducer,
     // inputLoginForm: inputLoginFormReducer,
+    homePage: homePageReducer,
+
   },
 });
